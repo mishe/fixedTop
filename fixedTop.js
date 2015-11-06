a286a3fad0d4ebe969b39621a4c56defc76179e8
mishe163 <chencaihua@hujiang.com>
2015-11-06 15:58:02 +0800

    固定位置展示JS

---
+++ fixedTop.js
@@ -0,0 +1,53 @@
+(function ($) {
+    $.extend($.fn, {
+        fixedTop: function (options) {
+            var opt = {
+                top: 0, //距离屏幕的上边距
+                zIndex: 0
+            }
+            opt = $.extend(opt, options);
+            this.each(function () {
+                var self = this,
+                    top = self.offsetTop,
+                    seftPos = self.getBoundingClientRect(),
+                    left=seftPos.left+(document.documentElement.scrollLeft || document.body.scrollLeft),
+                    selfHeight = seftPos.height,
+                    selfWidth = seftPos.width;
+                setInterval(function () {
+                    var st = document.documentElement.scrollTop || document.body.scrollTop,
+                        sl=document.documentElement.scrollLeft || document.body.scrollLeft,
+                        msl=$(document).width()-document.documentElement.clientWidth,
+                        pos = 0,
+                        height = 0;
+                    if (!pos && st - top + opt.top >= 0) {
+                        $(self).css({
+                            'position': 'fixed',
+                            'top': 0,
+                            'left':sl>0?left-sl:'auto',
+                            'padding-top': opt.top,
+                            'width': selfWidth,
+                            'z-index': opt.zIndex
+                        });
+                        pos = 1;
+                        if (document.documentElement.clientHeight - 180 < selfHeight) {
+                            height = $(document).height();
+                            if (st + selfHeight + 200 > height) {
+                                $(self).css({
+                                    'position': 'static',
+                                    'padding-top': 0
+                                });
+                            }
+                        }
+                    }
+                    else if (st - top <= 0) {
+                        $(self).css({
+                            'position': 'static',
+                            'padding-top': 0
+                        });
+                    }
+                }, 16);
+
+            })
+        }
+    });
+})($ || jQuery || Zepto);
